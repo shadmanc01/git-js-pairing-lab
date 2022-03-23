3caf3de21b6872ff5e66228fd031802954cc49a2
shadmanc01 <shadmanchowdhury01@gmail.com>
2022-03-23 18:02:55 +0000

    Q2

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function fivetoonehunbred(){
     }
 }
 fivetoonehunbred();
+
+function multiplesofThree() {
+    for (let i = 0; i <= 100; i ++) {
+        if(i % 3 === 0) {
+            console.log(i);
+        }
+    }
+}
+multiplesofThree();
