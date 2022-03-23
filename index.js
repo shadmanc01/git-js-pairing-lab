5f99a0637eafad022563c5ec54f332c2a53c4a9e
shadmanc01 <shadmanchowdhury01@gmail.com>
2022-03-23 19:54:11 +0000

    Q15

--- index.js
+++ index.js
@@ -120,3 +120,15 @@ function absoluteNineteen(num) {
 }
 }
 console.log(absoluteNineteen(20));
+
+function switchLetters(str1) 
+  {
+  if (str1.length <= 1)
+  {
+    return str1;
+  }
+  mid_char = str1.substring(1, str1.length - 1);
+  return (str1.charAt(str1.length - 1)) + mid_char + str1.charAt(0);
+}
+console.log(switchLetters('Shadman'));
+console.log(switchLetters('Aldair'));
